def5933ff10a96f7acf8397a7eddaab07246ecff
ryu0210 <89006072+ryu0210@users.noreply.github.com>
2023-01-18 11:28:46 +0700

    Create Enscapulation.java

---
+++ Enscapulation.java
@@ -0,0 +1,27 @@
+/** Encapsulation */
+
+/// Summary: Think of them as initializer (set variable to) in scratch, if no value is set
+/// there will be no information about the first variable, other words, the game's start would not be interactive
+
+
+class Person {
+    private String name; // private = restricted access
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+        myObj.setName("John"); // Set the value of the name variable to "John"
+        System.out.println(myObj.getName());
+    }
+}
