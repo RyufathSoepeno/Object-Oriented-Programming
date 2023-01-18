98dbf804f287b5723908702cc5013b90edbdb701
ryu0210 <89006072+ryu0210@users.noreply.github.com>
2023-01-18 11:31:30 +0700

    Update Week 1 - Polymorphism.java

--- Week 1 - Polymorphism.java
+++ Week 1 - Polymorphism.java
@@ -30,3 +30,60 @@ class Main {
         myDog.animalSound();
     }
 }
+
+
+/* ======================================================================================================== */
+
+public class Compile_Time {
+    // First addition function
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Second addition function
+    public static double add(
+            double a, double b)
+    {
+        return a + b;
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        // Here, the first addition
+        // function is called
+        System.out.println(add(2, 3));
+
+        // Here, the second addition
+        // function is called
+        System.out.println(add(2.0, 3.0));
+    }
+}
+
+/* ======================================================================================================== */
+
+class Test {
+
+    // Implementing a method
+    public void method()
+    {
+        System.out.println("Method 1");
+    }
+}
+
+public class Run_Time extends Test {
+    // Overriding the parent method
+    public void method()
+    {
+        System.out.println("Method 2");
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        Test test = new Run_Time();
+
+        test.method();
+    }
+}
