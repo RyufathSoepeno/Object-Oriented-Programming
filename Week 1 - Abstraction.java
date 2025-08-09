8c6296cebfbe2d2be2fa9689674a26dde1a5718f
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2025-08-09 10:42:17 +0700

    Update Week 1 - Abstraction.java

--- Week 1 - Abstraction.java
+++ Week 1 - Abstraction.java
@@ -3,6 +3,9 @@
 /// Summary: Data abstraction is the process of hiding certain details and showing only essential information to the user.
 /// Abstraction can be achieved with either abstract classes or interfaces
 
+/** Abstraction → Hiding the complex stuff and only showing the important details.
+Example: You use a TV remote without knowing how the circuits inside work. */
+
 /** https://www.instagram.com/p/Cskfp_gI0z0/?igshid=MmJiY2I4NDBkZg== */
 
 // Abstract class
